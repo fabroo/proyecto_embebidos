e12a05d2d0322cfa5318d77a9ac35ca3dd422058
fabroo <47898082+fabroo@users.noreply.github.com>
2021-11-04 11:55:45 -0300

    Update main.ino

--- hw/main.ino
+++ hw/main.ino
@@ -25,12 +25,12 @@ MFRC522::StatusCode status;
 // Defined pins to module RC522
 MFRC522 rfid = MFRC522(SS_PIN, RST_PIN);
 
-const int ipaddress[4] = {192,168,1,138};
+const int ipaddress[4] = {192, 168, 1, 138};
 
 //wifi
 #define port 80
-#define ssid "Hogar"
-#define psw "casacorzini10"
+#define ssid "ssid"
+#define psw "password"
 
 //hardware
 #define LED_VERDE 23
@@ -49,6 +49,12 @@ WebServer server(port);
 
 void setup(void) {
   Serial.begin(115200);
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmEnable(timer);
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, psw);
   Serial.println("");
@@ -69,12 +75,7 @@ void setup(void) {
 
   Serial.print(F("Reader :"));
   rfid.PCD_DumpVersionToSerial();
-if (rfid.PCD_PerformSelfTest()) Serial.println("Passed Self-Test");
-
-
-
-
-
+  if (rfid.PCD_PerformSelfTest()) Serial.println("Passed Self-Test");
 
 
   server.on(("/"), []() {
@@ -87,16 +88,21 @@ if (rfid.PCD_PerformSelfTest()) Serial.println("Passed Self-Test");
 
     if (gateId == gate_id) { // si es la misma que la que esta definida arriba
       if (valid(token)) {
-          isResident = true;
-          Serial.println("opening gate...");
-          server.send(200, "text/plain", "{err:false, msg:'opening gate...'}");
+        
+        // EMPEZAR A RECONOCER RFID, SI PASAN 20 SEGUNDOS SIN RECONOCER VUELVE A 0
+
+        // ENDL
+        
+        isResident = true;
+        Serial.println("opening gate...");
+        server.send(200, "text/plain", "{err:false, msg:'opening gate...'}");
       }
       else {
         server.send(400, "text/plain", "{err:true, msg:'invalid token'}");
       }
     }
-    else{
-        server.send(400, "text/plain", "{err:true, msg:'invalid gate_id'}");
+    else {
+      server.send(400, "text/plain", "{err:true, msg:'invalid gate_id'}");
     }
 
   });
@@ -109,42 +115,43 @@ void loop(void) {
   server.handleClient();
 
   //implement fingerprint
-  
-  switch(ESTADO){
+
+  switch (ESTADO) {
     case OPEN:
       openGate();
       isResident = false;
       ESTADO = CLOSE;
-    break;
+      break;
     case CLOSE:
-    closeGate();
-    ESTADO = REST;
-    break;
+      closeGate();
+      ESTADO = REST;
+      break;
     case REST:
-     if (isResident){
+
+      if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+        return;
+
+      String buffer;
+      for (byte byte = 0 ; byte < rfid.uid.size ; byte++)
+        buffer += String(rfid.uid.uidByte[byte] < 0x10 ? " 0" : " ") + String(rfid.uid.uidByte[byte], HEX);
+      Serial.println("Nuevo tag: " + buffer);
+
+      rfid.PICC_HaltA();
+      rfid.PCD_StopCrypto1();
+
+      if (isResident) {
         ESTADO = OPEN;
-     }
-     else{
-       // DO NOTHING
-     }
-     break;
+      }
+      else {
+        // DO NOTHING
+      }
+      break;
   }
 
-  //readRFID();
-if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
-    return;
-
-  String buffer;
-  for (byte byte = 0 ; byte < rfid.uid.size ; byte++)
-    buffer += String(rfid.uid.uidByte[byte] < 0x10 ? " 0" : " ") + String(rfid.uid.uidByte[byte], HEX);
-  Serial.println("NUID: " + buffer);
-
-  rfid.PICC_HaltA();
-  rfid.PCD_StopCrypto1();
-  delay(2);//allow the cpu to switch to other tasks
+  delay(2);
 }
 
-void openGate(){
+void openGate() {
   Serial.println("");
   Serial.println("<------------>");
   Serial.println("abriendo puerta");
@@ -153,7 +160,7 @@ void openGate(){
   Serial.println("<------------>");
 }
 
-void closeGate(){
+void closeGate() {
   Serial.println("");
   Serial.println("<------------>");
   Serial.println("cerrando puerta");
@@ -162,65 +169,10 @@ void closeGate(){
   Serial.println("<------------>");
 }
 
-bool valid(String token){
+bool valid(String token) {
   // implement validator
-  if (token.length() > 5){
+  if (token.length() > 5) {
     return true;
   }
   return false;
 }
-
-void readRFID(void ) { /* function readRFID */
-  ////Read RFID card
-
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-  // Look for new 1 cards
-  if ( ! rfid.PICC_IsNewCardPresent())
-  
-    return;
-
-  // Verify if the NUID has been readed
-  if (  !rfid.PICC_ReadCardSerial())
-    return;
-
-  // Store NUID into nuidPICC array
-  for (byte i = 0; i < 4; i++) {
-    nuidPICC[i] = rfid.uid.uidByte[i];
-  }
-
-  Serial.print(F("RFID In dec: "));
-  printDec(rfid.uid.uidByte, rfid.uid.size);
-  Serial.println();
-
-  // Halt PICC
-  rfid.PICC_HaltA();
-
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
-
-}
-
-
-/**
-   Helper routine to dump a byte array as hex values to Serial.
-*/
-void printHex(byte *buffer, byte bufferSize) {
-  
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
-}
-
-/**
-   Helper routine to dump a byte array as dec values to Serial.
-*/
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], DEC);
-  }
-}
-
