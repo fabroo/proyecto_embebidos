446edd67834a8d8037605dfece90e412c6bf987f
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2021-11-17 21:13:26 -0300

    funciona en mi compu

--- hw/main.ino
+++ hw/main.ino
@@ -2,6 +2,8 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
+#include <ArduinoJson.hpp>
+#include <ArduinoJson.h>
 #include <ESPmDNS.h>
 #include <uri/UriBraces.h>
 #include <uri/UriRegex.h>
@@ -13,11 +15,15 @@
 #define RST_PIN   22
 #define SIZE_BUFFER     18
 #define MAX_SIZE_BLOCK  16
-#define greenPin     12
-#define redPin       32
+#define VERDE     12
+#define ROJO       4
 
 byte nuidPICC[4] = {0, 0, 0, 0};
 
+volatile int interruptCounter;
+int totalInterruptCounter;
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 MFRC522::MIFARE_Key key;
 //authentication return status code
@@ -29,8 +35,8 @@ const int ipaddress[4] = {192, 168, 1, 138};
 
 //wifi
 #define port 80
-#define ssid "ssid"
-#define psw "password"
+#define ssid "Hogar"
+#define psw "casacorzini10"
 
 //hardware
 #define LED_VERDE 23
@@ -39,22 +45,39 @@ const int ipaddress[4] = {192, 168, 1, 138};
 //states
 #define CLOSE 0
 #define OPEN 1
+#define SEARCH 2
 #define REST 3
 
 //variables
 const String gate_id = "gate_id";
-int ESTADO = REST;
+int ESTADO = SEARCH;
+bool searching = false;
 bool isResident = false;
+String residentName = "alguien";
 WebServer server(port);
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  if(searching){
+    Serial.println("Terminó el timer. No se encontró RFID.");
+    searching = false;
+  }
+  
+  portEXIT_CRITICAL_ISR(&timerMux);
+
+}
+
+void IRAM_ATTR onTimer2() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  
+  portEXIT_CRITICAL_ISR(&timerMux);
+
+}
 
 void setup(void) {
   Serial.begin(115200);
-
-  timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 1000000, true);
-  timerAlarmEnable(timer);
-  
+  pinMode(ROJO, OUTPUT);
+  pinMode(VERDE, OUTPUT);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, psw);
   Serial.println("");
@@ -82,28 +105,34 @@ void setup(void) {
     server.send(200, "text/plain", "SERVIDOR EQUIPO PIOLA");
   });
 
-  server.on(UriBraces("/openGate/{}/{}"), []() {
-    String gateId = server.pathArg(0);
-    String token = server.pathArg(1);
+  server.on(UriBraces("/openGate"), HTTP_POST, []() {
 
-    if (gateId == gate_id) { // si es la misma que la que esta definida arriba
-      if (valid(token)) {
-        
-        // EMPEZAR A RECONOCER RFID, SI PASAN 20 SEGUNDOS SIN RECONOCER VUELVE A 0
+    if(ESTADO == REST) {
+      StaticJsonDocument<300> doc;
+      DeserializationError error = deserializeJson(doc, server.arg("plain"));
+      if (error) {
+        Serial.println("error wacho qsy");
+      }
+      String rfid1 = doc["rfidList"][0];
+      String rfid2 = doc["rfidList"][1];
+      String namee = doc["name"];
+      residentName = namee;
+  
+      // si es la misma que la que esta definida arriba
+      Serial.println("Empezando el reconocimiento...");
+      searching = true;
+      timer = timerBegin(0, 80, true);
+      timerAttachInterrupt(timer, &onTimer, true);
+      timerAlarmWrite(timer, 1500000, false);
+      timerAlarmEnable(timer);
+      ESTADO = SEARCH;
+      isResident = true;
+  
+      server.send(200, "text/plain", "Arrancando a reconocer.");
+    } else {
+      server.send(400, "text/plain", "Ya se está reconociendo bro.");
+    }
 
-        // ENDL
-        
-        isResident = true;
-        Serial.println("opening gate...");
-        server.send(200, "text/plain", "{err:false, msg:'opening gate...'}");
-      }
-      else {
-        server.send(400, "text/plain", "{err:true, msg:'invalid token'}");
-      }
-    }
-    else {
-      server.send(400, "text/plain", "{err:true, msg:'invalid gate_id'}");
-    }
 
   });
 
@@ -118,15 +147,19 @@ void loop(void) {
 
   switch (ESTADO) {
     case OPEN:
+      digitalWrite(VERDE, HIGH);
+      digitalWrite(ROJO, LOW);
       openGate();
       isResident = false;
       ESTADO = CLOSE;
       break;
     case CLOSE:
+      digitalWrite(VERDE, LOW);
+      digitalWrite(ROJO, HIGH);
       closeGate();
       ESTADO = REST;
       break;
-    case REST:
+    case SEARCH:
 
       if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
         return;
@@ -135,38 +168,40 @@ void loop(void) {
       for (byte byte = 0 ; byte < rfid.uid.size ; byte++)
         buffer += String(rfid.uid.uidByte[byte] < 0x10 ? " 0" : " ") + String(rfid.uid.uidByte[byte], HEX);
       Serial.println("Nuevo tag: " + buffer);
+      Serial.println();
 
       rfid.PICC_HaltA();
       rfid.PCD_StopCrypto1();
-
-      if (isResident) {
+      //cambiar el if a que busque un buffer concreto
+      if (isResident && buffer == buffer) {
+        searching = false;
+        Serial.print("RFID encontrado. Bienvenido, ");
+        Serial.println(residentName);
         ESTADO = OPEN;
       }
       else {
         // DO NOTHING
       }
       break;
+
+
   }
 
   delay(2);
 }
 
 void openGate() {
-  Serial.println("");
-  Serial.println("<------------>");
   Serial.println("abriendo puerta");
   delay(2000);
   Serial.println("se termino de abrir puerta");
-  Serial.println("<------------>");
+
 }
 
 void closeGate() {
-  Serial.println("");
-  Serial.println("<------------>");
   Serial.println("cerrando puerta");
   delay(2000);
   Serial.println("se termino de cerrar puerta");
-  Serial.println("<------------>");
+
 }
 
 bool valid(String token) {
---
+++ python/main.py
@@ -0,0 +1,103 @@
+import time
+from typing import Collection
+import cv2
+import sys
+import requests
+from time import sleep
+import boto3
+import warnings
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('firebase.json')
+firebase_admin.initialize_app(cred)
+
+warnings.filterwarnings("ignore")
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+checking = True
+client = boto3.client('rekognition')
+url = "192.168.1.138"
+print("Detectando caras.")
+
+while True:
+    video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while checking:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        if(len(faces) > 0):
+            
+            for (x, y, w, h) in faces:
+                if w > 100 and h > 100:
+                    cv2.imwrite("../sendImage.png", frame)
+                    checking = False
+
+        cv2.imshow('my webcam', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+    print("Cara detectada, intentando reconocer.")
+
+    class RekognitionImage:
+
+        def __init__(self, image, image_name, rekognition_client):
+
+            self.image = image
+            self.image_name = image_name
+            self.rekognition_client = rekognition_client
+
+        @classmethod
+        def from_file(cls, image_file_name, rekognition_client, image_name=None):
+            with open(image_file_name, 'rb') as img_file:
+                image = {'Bytes': img_file.read()}
+            name = image_file_name if image_name is None else image_name
+            return cls(image, name, rekognition_client)
+
+        def search_faces(self):
+            res = client.search_faces_by_image(
+                CollectionId="1a2b3c",
+                Image=self.image
+
+            )
+            faces = res["FaceMatches"]
+            arr = []
+            for face in faces:
+                arr.append(face['Face']["ExternalImageId"])
+            if len(list(set(arr))) == 1:
+                dni = arr[0]
+                db = firestore.client()
+                sn = db.collection(u'users').where(u'dni', u'==', dni).stream()
+
+                for doc in sn:
+                    name = doc.to_dict()['nombre']
+
+                print("Cara reconocida, " + name)
+                print("Use su llave rfid para ingresar.")
+                
+                requests.post("http://" + url + "/openGate", json={
+                    "rfidList": ["08 3d 92 08", "08 ec c8 1b"],
+                    "name": name
+                })
+
+    image = RekognitionImage.from_file('../sendImage.png', client)
+    image.search_faces()
+    time.sleep(20)
